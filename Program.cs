5eeccefe9556b44baf9a52ad1de299878560f506
vspl3sk <vspl3sk@yandex.ru>
2022-07-15 16:38:44 +0300

    Задача на определение выходного дня по вводу числа, обозначающего день недели

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber < 1 || dayNumber > 7)
+{
+    Console.Write("Введите значение от 1 до 7");
+}
+else if (dayNumber == 6 || dayNumber == 7) Console.WriteLine("Этот день является выходным.");
